f6991bb9ce6ffcdc29097550896228c0fa081175
Sam Selikoff <sam.selikoff@gmail.com>
2019-05-30 17:24:23 -0400

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
+import './App.css';
+
+function Stat({ title = 'Stat', value = 5 }) {
+  return (
+    <div className="w-1/2 h-1/2 uppercase leading-none flex flex-col justify-center">
+      <p className="text-6xl font-semibold text-white">{value}</p>
+      <p className="text-xl pt-3 font-semibold text-gray-600">{title}</p>
+    </div>
+  );
+}
+
+function Repo({ name }) {
+  return (
+    <div className="bg-gray-800 text-white w-1/2 h-1/2 p-8 shadow-2xl rounded flex flex-col">
+      <p className="font-semibold text-3xl text-white">{name}</p>
+
+      <div className="text-xl mt-3 flex-1 flex flex-wrap">
+        <Stat title="Untriaged issues" value={28} />
+        <Stat title="Bugs" value={400} />
+        <Stat title="Dependabot PRs" value={3} />
+        <Stat title="NPM downloads" value={'40k'} />
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="flex bg-gray-700 w-screen h-screen">
-      <header>EmberMap Dashboard</header>
-      <main className="bg-white">
-        <p className="text-white font-bold text-5xl" />
+    <div className="flex bg-gray-900 w-screen h-screen flex flex-col">
+      <header className="bg-gray-800 text-gray-600 px-8 py-4 text-3xl">
+        EmberMap Dashboard
+      </header>
+      <main className="flex-1 p-16 flex items-center justify-center">
+        <Repo name="ember-cli-mirage" />
       </main>
     </div>
   );
