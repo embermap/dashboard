a45cc0c8aef4f1c32b98fa26e308cad87fb19544
Sam Selikoff <sam.selikoff@gmail.com>
2019-05-30 16:34:39 -0400

    Add Prettier

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='flex items-center justify-center bg-gray-700 w-screen h-screen'>
-      <p className='text-white font-bold text-5xl'>
-        EmberMap Dashboard
-      </p>
+    <div className="flex bg-gray-700 w-screen h-screen">
+      <header>EmberMap Dashboard</header>
+      <main className="bg-white">
+        <p className="text-white font-bold text-5xl" />
+      </main>
     </div>
   );
 }
